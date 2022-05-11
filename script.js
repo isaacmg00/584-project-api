231e321838b54d45fe76921e8c4a5625e469b151
Isaac Garcia <isaacmg123@gmail.com>
2022-05-10 20:52:52 -0700

    added popmotion effects

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+const { easing, keyframes, styler } = window.popmotion;
+
+const divStyler = styler(document.querySelector(".box"));
+
+keyframes({
+  values: [
+    { x: 0, y: 0, rotateY: 0, background: "red" },
+    { x: 400, y: 0, rotateY: 180, rotateX: 0, background: "red" },
+    { x: 0, y: 0, rotateY: 0, rotateX: 0, background: "#9B65DE" },
+  ],
+  duration: 5000,
+  easings: [easing.easeInOut, easing.easeInOut],
+  loop: Infinity,
+}).start(divStyler.set);
+
 const year = new Date().getFullYear();
 let month = new Date().getUTCMonth() + 1;
 month = "0" + month;
 let day = new Date().getDate();
-let homeTeamTag;
 const urlDate = year + month + day;
 let numGames = 0;
 let [gamesList, homeTeams, awayTeams, homeScores, awayScores] = [
@@ -23,13 +37,9 @@ promise
   })
   .then(function (processedResponse) {
     const numGames = processedResponse.numGames;
-    console.log(numGames);
-
     const htmlHeading = document.getElementById("heading");
     htmlHeading.innerText = `There are ${numGames} games being played today`;
     for (let i = 0; i < numGames; i++) {
-      console.log(i);
-
       currentGame = processedResponse.games[i];
       gamesList[i] = currentGame;
       homeTeams[i] = currentGame.hTeam.triCode;
@@ -43,17 +53,10 @@ promise
       let element = document.getElementById(divID);
       element.classList.add("game");
     }
-    const games = processedResponse.games[0];
-    console.log(gamesList);
-    console.log(homeTeams);
-    console.log(awayTeams);
-    console.log(homeScores);
-    console.log(awayScores);
     return numGames;
   })
   .then(function (numGames) {
     try {
-      console.log(numGames);
       for (let i = 0; i < numGames; i++) {
         let homeTeamTag = document.getElementById(`game${i}home`);
         homeTeamTag.innerText = `${homeTeams[i]} (HOME): ${homeScores[i]}`;
